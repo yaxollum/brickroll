a4f00a28a1bc4f2a980529f87757b3e2e43ae155
Peter Ye <yaxollum@gmail.com>
2023-02-25 22:35:56 -0500

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs::read_to_string;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let print_help = || eprintln!("Usage: {} <file>", args[0]);
+    if args.len() == 2 {
+        if args[1] == "-h" || args[1] == "--help" {
+            print_help();
+        } else {
+            if let Ok(bf) = read_to_string(&args[1]) {
+                println!("{}", bf);
+            } else {
+                eprintln!("Unable to read file \"{}\"", args[1]);
+                process::exit(1);
+            }
+        }
+    } else {
+        print_help();
+        process::exit(1);
+    }
 }
